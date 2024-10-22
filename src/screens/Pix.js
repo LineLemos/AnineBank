8b571e172471e7a0b0c8c0be5ef897e4797b09c3
Aline Lemos <alinelemos@id.uff.br>
2024-10-22 17:58:37 -0300

    valor do pix em formatCurrency

--- src/screens/Pix.js
+++ src/screens/Pix.js
@@ -1,53 +1,62 @@
 import { useState } from "react";
 import { TextInput, View, Text, Button, StyleSheet } from "react-native";
 
-export default function Pix({route, navigation}){
+export default function Pix({ route, navigation }) {
+  const { saldo, setSaldo } = route.params;
+  const [valorPix, setValorPix] = useState("");
+  const [chavePix, setChavePix] = useState("");
 
-    const {saldo, setSaldo} = route.params;
-    const [valorPix, setValorPix] = useState("");
-    const [chavePix, setChavePix] = useState("");
+  const formatCurrency = (value) => {
+    const cleanedValue = value.replace(/\D/g, "");
+    const formattedValue = (cleanedValue / 100).toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    return formattedValue;
+  };
 
+  const handleInputChange = (value) => {
+    setValorPix(formatCurrency(value));
+  };
 
-    
-    
-    const sendPix = () => { 
-        const valor = parseFloat(valorPix);
+  const sendPix = () => {
+    const valor = parseFloat(valorPix.replace(/[^\d.-]/g, ""));
 
-        if(valor <= saldo){
-            setSaldo(saldo - valor)
-            navigation.goBack()
-
-        }else{
-            alert("Saldo insuficiente.")
-        }
+    if (valor <= saldo) {
+      setSaldo(saldo - valor);
+      navigation.goBack();
+    } else {
+      alert("Saldo insuficiente.");
     }
-    return (
-      <View style={styles.container}>
-        <Text style={styles.label}>Chave PIX:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite a chave PIX"
-          placeholderTextColor="#737373"
-          value={chavePix}
-          onChangeText={setChavePix}
-        />
-        <Text style={styles.label}>Valor do PIX:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite o valor"
-          placeholderTextColor="#737373"
-          value={valorPix}
-          keyboardType="numeric"
-          onChangeText={setValorPix}
-        />
-        <Button
-          style={styles.button}
-          title="Enviar PIX"
-          onPress={sendPix}
-          color="#6b21a8"
-        />
-      </View>
-    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Chave PIX:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a chave PIX"
+        placeholderTextColor="#737373"
+        value={chavePix}
+        onChangeText={setChavePix}
+      />
+      <Text style={styles.label}>Valor do PIX:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o valor"
+        placeholderTextColor="#737373"
+        value={valorPix}
+        keyboardType="numeric"
+        onChangeText={handleInputChange}
+      />
+      <Button
+        style={styles.button}
+        title="Enviar PIX"
+        onPress={sendPix}
+        color="#6b21a8"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -60,7 +69,7 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 18,
     marginVertical: 10,
-    color:"#fff",
+    color: "#fff",
   },
   input: {
     borderWidth: 1,
@@ -68,8 +77,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginVertical: 10,
-    color:"#fff",
+    color: "#fff",
   },
-
-
 });
