87c3e8e87fd477a97f25e8eb65035304c6e9f53b
Aline Lemos <alinelemos@id.uff.br>
2024-10-22 18:02:12 -0300

    #000

--- src/screens/Pix.js
+++ src/screens/Pix.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     justifyContent: "center",
-    backgroundColor: "#262626",
+    backgroundColor: "#000",
   },
   label: {
     fontSize: 18,
